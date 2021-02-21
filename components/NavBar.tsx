e8e5e9118764e1c8952bdbbe011aea2254dcf67f
CHOEJANGHYEOK <jh30056@gmail.com>
2021-02-21 17:50:54 +0900

    Change detail of animation and text color of nav bar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,7 +4,7 @@ const NavBar = () => {
     return (
         <div className="z-30 fixed flex md:justify-center font-roboto-mono text-2xl md:text-xl font-light md:font-bold
                             top-10 right-10 md:top-auto md:right-0 md:w-1/12 md:bg-secondary md:h-screen">
-            <nav className="flex flex-col md:justify-between h-5/6 my-auto">
+            <nav className="flex flex-col md:justify-between h-5/6 my-auto text-white">
                 <Link href="/work">
                     <a className="ml-auto md:mx-auto hover:text-point">work</a>
                 </Link>
