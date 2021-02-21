9b1ca8bb4dc6c6d868c08ee76c50bde6ea43e32e
CHOEJANGHYEOK <jh30056@gmail.com>
2021-02-22 01:37:35 +0900

    Change font size and color of nav bar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link'
 
 const NavBar = () => {
     return (
-        <div className="z-30 fixed flex md:justify-center font-roboto-mono text-2xl md:text-xl font-light md:font-bold
+        <div className="z-30 fixed flex md:justify-center font-roboto-mono text-2xl md:text-xl font-light
                             top-10 right-10 md:top-auto md:right-0 md:w-1/12 md:bg-secondary md:h-screen">
-            <nav className="flex flex-col md:justify-between h-5/6 my-auto text-white">
+            <nav className="flex flex-col md:justify-between h-5/6 my-auto md:text-white">
                 <Link href="/work">
                     <a className="ml-auto md:mx-auto hover:text-point">work</a>
                 </Link>
